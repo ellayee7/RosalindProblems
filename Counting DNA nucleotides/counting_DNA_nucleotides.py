3e2192aaff54e706502e2848e160efc60693c5c7
ellayee7 <ellayee13@gmail.com>
2025-02-25 21:10:13 -0800

    initial

---
+++ Counting DNA nucleotides/counting_DNA_nucleotides.py
@@ -0,0 +1,22 @@
+"""
+Given: A DNA string s of length at most 1000 nt.
+
+Return: Four integers (separated by spaces) counting the respective number of times that 
+the symbols 'A', 'C', 'G', and 'T' occur in s.
+"""
+
+def countDNAnuc(seq)
+    A = 0
+    C = 0
+    G = 0
+    T = 0
+    for c in seq:
+        if c = 'A':
+            A += 1
+        if c = 'C':
+            C += 1
+        if c = 'G':
+            G += 1
+        if c = 'T':
+            T += 1
+    return f'{A} {C} {G} {T}'
