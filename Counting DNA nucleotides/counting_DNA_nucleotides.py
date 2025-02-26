a1e763dcfbaf2fbbaec039521b89495b0e1d2d60
ellayee7 <ellayee13@gmail.com>
2025-02-25 21:20:23 -0800

    counting DNA nucleotides

--- Counting DNA nucleotides/counting_DNA_nucleotides.py
+++ Counting DNA nucleotides/counting_DNA_nucleotides.py
@@ -5,18 +5,23 @@ Return: Four integers (separated by spaces) counting the respective number of ti
 the symbols 'A', 'C', 'G', and 'T' occur in s.
 """
 
-def countDNAnuc(seq)
+def countDNAnuc(seq):
     A = 0
     C = 0
     G = 0
     T = 0
     for c in seq:
-        if c = 'A':
+        if c == 'A':
             A += 1
-        if c = 'C':
+        if c == 'C':
             C += 1
-        if c = 'G':
+        if c == 'G':
             G += 1
-        if c = 'T':
+        if c == 'T':
             T += 1
     return f'{A} {C} {G} {T}'
+
+if __name__ == '__main__':
+    with open('rosalind_dna.txt', 'r') as file:
+        sequence = file.read().strip()
+    print(countDNAnuc(sequence))
