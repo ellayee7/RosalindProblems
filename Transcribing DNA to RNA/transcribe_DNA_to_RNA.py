53c41a5b114bf9fb418b2ea1b03d9ce5065e4ebd
ellayee7 <ellayee13@gmail.com>
2025-02-25 21:37:12 -0800

    transcribe DNA to RNA

---
+++ Transcribing DNA to RNA/transcribe_DNA_to_RNA.py
@@ -0,0 +1,20 @@
+"""
+Given a DNA string t corresponding to a coding strand, its transcribed RNA string u 
+is formed by replacing all occurrences of 'T' in t with 'U' in u.
+
+Given: A DNA string t
+ having length at most 1000 nt.
+
+Return: The transcribed RNA string of t.
+"""
+
+def transcribe(dna_seq):
+    rna_seq = dna_seq.replace('T', 'U')
+    return rna_seq
+
+if __name__ == '__main__':
+    with open('rosalind_rna.txt', 'r') as file:
+        sequence = file.read().strip()
+    print(transcribe(sequence))
+
+
