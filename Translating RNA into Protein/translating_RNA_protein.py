98327f6009ec4e012faadb0ac711394ac7eddcb3
ellayee7 <ellayee13@gmail.com>
2025-03-04 22:03:18 -0800

    update

--- Translating RNA into Protein/translating_RNA_protein.py
+++ Translating RNA into Protein/translating_RNA_protein.py
@@ -5,54 +5,39 @@ Return: The protein string encoded by s.
 """
 
 def translate(s):
-    codondict = {}
     ind = 0
     proteins = ''
+    codon_dict = {'UUU': 'F', 'UUC': 'F', 
+    'CUU': 'L', 'CUC': 'L', 'UUA': 'L', 'CUA': 'L', 'UUG': 'L', 'CUG': 'L', 
+    'AUG': 'M', 
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V', 
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S', 
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P', 
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T', 
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A', 
+    'UAU': 'Y', 'UAC': 'Y', 
+    'CAU': 'H', 'CAC': 'H', 
+    'AAU': 'N', 'AAC': 'N', 
+    'GAU': 'D', 'GAC': 'D', 
+    'UAA': 'Stop', 'UAG': 'Stop', 'UGA': 'Stop', 
+    'CAA': 'Q', 'CAG': 'Q', 
+    'AAA': 'K', 'AAG': 'K', 
+    'GAA': 'E', 'GAG': 'E', 
+    'UGU': 'C', 'UGC': 'C', 
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 
+    'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R', 
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G', 
+    'UGG': 'W',
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I'}
+
     while ind < len(s):
         if (ind+1) % 3 == 0 and ind != 0:
             codon = s[ind - 2: ind + 1]
-            if codon == 'UUU' or codon == 'UUC':
-                proteins += 'F'
-            elif codon == 'UUA' or codon == 'UUG' or codon == 'CUU' or codon == 'CUC' or codon == 'CUA' or codon == 'CUG':
-                proteins += 'L'
-            elif codon == 'UCU' or codon == 'UCC' or codon == 'UCA' or codon == 'UCG' or codon == 'AGU' or codon == 'AGC':
-                proteins += 'S'
-            elif codon == 'UAU' or codon == 'UAC':
-                proteins += 'Y'
-            elif codon == 'UGU' or codon == 'UGC':
-                proteins += 'C'
-            elif codon == 'UGG':
-                proteins += 'W'
-            elif codon == 'CCU' or codon == 'CCC' or codon == 'CCA' or codon == 'CCG':
-                proteins += 'P'
-            elif codon == 'CAU' or codon == 'CAC':
-                proteins += 'H'
-            elif codon == 'CAA' or codon == 'CAG':
-                proteins += 'Q'
-            elif codon == 'CGU' or codon == 'CGC' or codon == 'CGA' or codon == 'CGG' or codon == 'AGA' or codon == 'AGG':
-                proteins += 'R'
-            elif codon =='AUU' or codon == 'AUC' or codon == 'AUA':
-                proteins += 'I'
-            elif codon == 'AUG':
-                proteins += 'M'
-            elif codon == 'ACU' or codon == 'ACC' or codon == 'ACA' or codon == 'ACG':
-                proteins += 'T'
-            elif codon == 'AAU' or codon == 'AAC':
-                proteins += 'N'
-            elif codon == 'AAA' or codon == 'AAG':
-                proteins += 'K'
-            elif codon == 'GUU' or codon == 'GUC' or codon == 'GUA' or codon == 'GUG':
-                proteins += 'V'
-            elif codon == 'GCU' or codon == 'GCC' or codon == 'GCA' or codon == 'GCG':
-                proteins += 'A'
-            elif codon == 'GAU' or codon == 'GAC':
-                proteins += 'D'
-            elif codon == 'GAA' or codon == 'GAG':
-                proteins += 'E'
-            elif codon == 'GGU' or codon == 'GGC' or codon == 'GGA' or codon == 'GGG':
-                proteins += 'G'
-            elif codon == 'UAA' or codon == 'UAG' or codon == 'UGA':
-                return proteins
+            if codon in codon_dict:
+                if codon_dict[codon] == 'Stop':
+                    return proteins
+                else:
+                    proteins += codon_dict[codon]
             ind += 1
         else:
             ind += 1
